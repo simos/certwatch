13957bf729ea88bd4df29909e11466bae4ca5feb
Simos Xenitellis <simos@gnome.org>
2010-03-01 17:54:38 +0200

    Initial import

---
+++ defaults/preferences/certwatch.js
@@ -0,0 +1,5 @@
+pref("extensions.certwatch.boolpref", false);
+pref("extensions.certwatch.intpref", 0);
+pref("extensions.certwatch.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.certwatch@simos.info.description", "chrome://certwatch/locale/certwatch.properties");
