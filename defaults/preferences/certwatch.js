186502e30bc45b9b7422681699c2039a1bd2ef86
Simos Xenitellis <simos@gnome.org>
2010-08-05 01:12:02 +0300

    Removed obsolete files.

--- defaults/preferences/certwatch.js
+++ defaults/preferences/certwatch.js
@@ -1,4 +1,5 @@
 pref("extensions.certwatch.show_root_certificate", 1); //  -1: always, 0: disable, >=1 number of times
+pref("extensions.certwatch.show_intermediate_certificate", 1); //  -1: always, 0: disable, >=1 number of times
 pref("extensions.certwatch.show_website_certificate", 1); //  -1: always, 0: disable, >=1 number of times
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.certwatch@simos.info.description", "chrome://certwatch/locale/certwatch.properties");
